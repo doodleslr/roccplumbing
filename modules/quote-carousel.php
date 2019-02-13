afe7a75b2400bdd5a48b7a68f3541392d6f4a4f7
Daniel <dglarocca@hotmail.com>
2019-02-13 21:59:11 +1100

    Add files via upload

---
+++ modules/quote-carousel.php
@@ -0,0 +1,34 @@
+<div class="wrapper" id="carouselId">
+	<!-- start carousel here-->
+	<div class="carousel slide carousel-fade col-md-10 col-md-offset-1" data-interval="5000" data-ride="carousel" id="qt-carousel">
+		<div class="carousel-inner" id="quote-carousel">
+	<!--quote one-->
+			<div class="item active">
+				<p>Absolutely love you guys. Always a pleasure coming in. Loved the beautiful creative job you did on my hair. Xxoo</p>
+				<div class="rev-cont">
+					<span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><br>
+					<h4 class="textGrand">Denise Khoury</h4>
+				</div>
+				<img src="images/review/review_4.jpg" class="cust-img" alt="customer image" href="#"/>
+			</div>
+			<!--quote two-->
+			<div class="item">
+				<p>Great staff and beautiful hair!!! So hard to find such a TOP quality hairdresser in Sydney area.</p>
+				<div class="rev-cont">
+					<span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><br>
+					<h4 class="textGrand">Laura Menny</h4>
+				</div>
+				<img src="images/review/review_7.jpg" class="cust-img" alt="customer image" href="#"/>
+			</div>
+			<!--quote three-->
+			<div class="item">
+				<p>The up class treatment you can finally trust. I just wanted someone who would get the job done just the way I wanted. Thanks Carl!</p>
+				<div class="rev-cont">
+					<span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><span class="cc-star"</span><br>
+					<h4 class="textGrand">Espy H. Rasheed</h4>
+				</div>
+				<img src="images/review/review_10.jpg" class="cust-img" alt="customer image" href="#"/>
+			</div>
+		</div><!--end inner carousel-->
+	</div><!-- end carousel here-->
+</div><!--end wrapper-->
